0e8beee02d6c002323f38ff0878be50255075564
EMPIRE-DUCK <62853075+EMPIRE-DUCK@users.noreply.github.com>
2020-05-20 17:44:03 +0500

    Add files via upload

---
+++ ВИДОС/ВИДОС/coded.php
@@ -0,0 +1,3 @@
+<title>Сайт предоставлен каналом Лютый</title>
+<li>Сайт предоставлен каналом Лютый</li>
+<li>По всем вопросам <input type="button" onclick="window.location.href = 'https://vk.com/aleksandrkirichek';" value="Александр"/>
